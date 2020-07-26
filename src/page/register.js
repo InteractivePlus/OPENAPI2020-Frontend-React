7cb2480441536fdf478f46afef35b7298fb39d7b
zHElEARN <zhe_learn@liuzeyun.tech>
2020-07-26 09:59:30 +0800

    add logo

--- src/page/register.js
+++ src/page/register.js
@@ -1,8 +1,39 @@
 import React from "react";
 
+import { CssBaseline, Container, Grid } from "@material-ui/core";
+
 class Register extends React.Component {
 	render() {
-		return <div>Register</div>;
+		return (
+			<>
+				<CssBaseline />
+				<Container maxWidth="md">
+					<Grid container>
+						<Grid item xs={12}>
+							Logo
+						</Grid>
+						<Grid item xs={12}>
+							Tip
+						</Grid>
+						<Grid item xs={12}>
+							Register Tip
+						</Grid>
+						<Grid item xs={12}>
+							Form
+						</Grid>
+						<Grid item xs={4}>
+							Register Button
+						</Grid>
+						<Grid item xs={4}>
+							Password Reset
+						</Grid>
+						<Grid item xs={4}>
+							Login
+						</Grid>
+					</Grid>
+				</Container>
+			</>
+		);
 	}
 }
 
